7a6c5d22fad289e9d545d317e21b8d5bc0faa03f
Maor Tovim <maortovim@gmail.com>
2019-06-06 16:23:04 +0300

    add input function

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -3,6 +3,8 @@
 //    f: drawImg(gMeme)
 // takes gMeme
 // draw it according to size 
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
 
 function createCanvas() {
     let canvasWidth = getCanvasWidth()
@@ -31,41 +33,43 @@ function getCanvasWidth() {
 
 function create() {
 
-    var canvas = document.querySelector('#canvas')
+    
     var canvasWidth = 300;
     var canvasHeight = canvasWidth / 4 * 3
 
     var ctx = canvas.getContext('2d')
 
-    debugger
+    // debugger
     canvas.width = canvasWidth
     canvas.height = canvasHeight
 
-    let memeImgUrl = getImgUrlbyId()
+    // let memeImgUrl = getImgUrlbyId()
 
-    const image = new Image(canvasWidth, canvasHeight);
-    image.src = memeImgUrl
-    image.onload = ((image) => {
-        ctx.drawImage(image, 0, 0);
-    })
-
-
-
-
-    function drawImG() {
-
-
-
-
-
-    }
-
-
-
+    drawImg('https://www.outsideonline.com/sites/default/files/styles/full-page/public/2019/04/02/mount-everest-climbers-stories_h.jpg?itok=QOk9XQrU');
 
     ctx.fillStyle = "#FF0000";
 
     ctx.fillRect(20, 20, 150, 100);
 
+    function drawImg(imgUrl) {
+        var img = new Image;
+        img.onload = function(){
+          ctx.drawImage(img,0,0,canvas.width, canvas.height ); 
+        };
+        img.src = imgUrl;
+    }
 
+}
+
+function drawText(txt,x,y) {
+    ctx.fillStyle = 'white'
+    ctx.strokeStyle = 'white'
+    ctx.font = "35px Arial";
+    ctx.fillText(txt, x, y);
+    ctx.strokeText(txt, x, y);
+}
+
+
+function onTypeText(txt) {
+drawText(txt,100,100);
 }
