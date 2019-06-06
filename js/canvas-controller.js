c51ef0c90660a2c01d7903e62fe4a5c5c0a75a7e
Yonatan Galili <yon.galili@gmail.com>
2019-06-06 15:00:01 +0300

    Yonatan 1500

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -2,3 +2,58 @@
 //    f: drawImg(gMeme)
 // takes gMeme
 // draw it according to size 
+'use strict'
+
+function onCanavasInit() {
+
+    getCanvasWidth()
+    create()
+
+}
+
+
+function getCanvasWidth() {
+    let MARGIN = 20;
+    return window.innerWidth - 2 * MARGIN;
+}
+
+function create() {
+
+    var canvas = document.querySelector('#canvas')
+    var canvasWidth = 300;
+    var canvasHeight = canvasWidth / 4 * 3
+
+    var ctx = canvas.getContext('2d')
+
+    debugger
+    canvas.width = canvasWidth
+    canvas.height = canvasHeight
+
+    let memeImgUrl = getImgUrlbyId()
+
+    const image = new Image(canvasWidth, canvasHeight);
+    image.src = memeImgUrl
+    image.onload = ((image) => {
+        ctx.drawImage(image, 0, 0);
+    })
+
+
+
+
+    function drawImG() {
+
+
+
+
+
+    }
+
+
+
+
+    ctx.fillStyle = "#FF0000";
+
+    ctx.fillRect(20, 20, 150, 100);
+
+
+}
--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,2 +1,10 @@
+function createCanvas() {
+    let canvasWidth = getCanvasWidth()
 
-    
+}
+
+
+function getCanvasWidth() {
+    let MARGIN = 20;
+    return window.innerWidth - 2 * MARGIN;
+}
